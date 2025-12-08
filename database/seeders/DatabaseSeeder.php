3758f38e319247729d2d1a41521117ab8fb43d30
COCOUVI ALEXANDRO <cocouvialexandro74@gmail.com>
2025-12-08 09:25:44 +0100

    final work

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,9 +23,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
- //          RoleSeeder::class,
+            //          RoleSeeder::class,
 //             RegionSeeder::class,
-             UserSeeder::class
+            LangueSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
